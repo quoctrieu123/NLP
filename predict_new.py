2c3434e8086b36316c8884f0de864d1ac89bc442
quoctrieu123 <quoctk04@gmail.com>
2025-03-27 00:18:19 +0700

    new_commit

--- predict_new.py
+++ predict_new.py
@@ -3,11 +3,11 @@ import torch
 import warnings
 warnings.simplefilter("ignore", category=FutureWarning)
 from data_preprocess import input_processor, preprocess, predict_sentence
-link_to_tokenize_dataset = r"C:\Users\Admin\Downloads\NLP\Model_NLP\Testmodel\dataset_for_tokenize.csv" #chủ động thay đổi đường dẫn tới file
-link_to_in_embedding = r"C:\Users\Admin\Downloads\NLP\Model_NLP\Testmodel\in_embedding.npy" #chủ động thay đổi đường dẫn tới file
-link_to_out_embedding = r"C:\Users\Admin\Downloads\NLP\Model_NLP\Testmodel\out_embedding.npy" #chủ động thay đổi đường dẫn tới file
+link_to_tokenize_dataset = r"C:\Users\Admin\Downloads\NLP\Model_NLP\NLP\dataset_for_tokenize.csv" #chủ động thay đổi đường dẫn tới file
+link_to_in_embedding = r"C:\Users\Admin\Downloads\NLP\Model_NLP\in_embedding.npy" #chủ động thay đổi đường dẫn tới file
+link_to_out_embedding = r"C:\Users\Admin\Downloads\NLP\Model_NLP\out_embedding.npy" #chủ động thay đổi đường dẫn tới file
 model = create_model(link_to_in_embedding,link_to_out_embedding) #load model đã train
-model.load_state_dict(torch.load(r"C:\Users\Admin\Downloads\NLP\Model_NLP\Testmodel\best_model.pth")) #chủ động thay đổi đường dẫn tới file
-input_sentence = "i love you so muchhh ."
+model.load_state_dict(torch.load(r"C:\Users\Admin\Downloads\NLP\Model_NLP\best_model.pth")) #chủ động thay đổi đường dẫn tới file
+input_sentence = "i wants eat meat ."
 output_sentence = predict_sentence(model,input_sentence,link_to_tokenize_dataset)
 print("Câu đã sửa:", output_sentence)
